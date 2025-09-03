7d87ee3face6e8d8e5c5d2f3d40d43628d28cba5
InfiniteLoop <44796943+InfiniteLoopGameDev@users.noreply.github.com>
2025-09-03 14:02:28 +0100

    Pass shader capability to slangc
    Will ensure that shaders are always compatible with Vulkan 1.2

--- cmake/SlangUtils.cmake
+++ cmake/SlangUtils.cmake
@@ -1,10 +1,13 @@
 # Modified from https://thatonegamedev.com/cpp/cmake/how-to-compile-shaders-with-cmake/#graphics-programming-with-vulkan
 
-function(add_shaders TARGET_NAME)
-    set(SHADER_SOURCE_FILES ${ARGN}) # the rest of arguments to this function will be assigned as shader source files
+function(slang_compile_spirv)
+    set(oneValueArgs TARGET_NAME)
+    set(multiValueArgs SOURCE_FILES CAPABILITIES)
+    cmake_parse_arguments(ADD_SHADER "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     # Validate that source files have been passed
-    list(LENGTH SHADER_SOURCE_FILES FILE_COUNT)
+    message(STATUS)
+    list(LENGTH ADD_SHADER_SOURCE_FILES FILE_COUNT)
     if (FILE_COUNT EQUAL 0)
         message(FATAL_ERROR "Cannot create a shaders target without any source files")
     endif ()
@@ -12,7 +15,7 @@ function(add_shaders TARGET_NAME)
     set(SPIRV_DIR ${CMAKE_CURRENT_BINARY_DIR}/spirv)
     file(MAKE_DIRECTORY ${SPIRV_DIR})
 
-    foreach (SHADER_SOURCE IN LISTS SHADER_SOURCE_FILES)
+    foreach (SHADER_SOURCE IN LISTS ADD_SHADER_SOURCE_FILES)
         cmake_path(ABSOLUTE_PATH SHADER_SOURCE NORMALIZE)
         cmake_path(GET SHADER_SOURCE STEM SHADER_NAME)
 
@@ -23,11 +26,14 @@ function(add_shaders TARGET_NAME)
             set(SLANG_OPTIMIZE_LEVEL 3)
         endif ()
 
+        # Join the capabilities into a single string
+        string(JOIN "+" CAPABILITIES "${ADD_SHADER_CAPABILITIES}")
 
         # Build command
         set(SHADER_COMMAND COMMAND slang::slangc -target spirv -O${SLANG_OPTIMIZE_LEVEL}
                 -fvk-use-entrypoint-name -source-embed-name ${SHADER_NAME}
-                -source-embed-style u32 -o ${SPIRV_DIR}/${SHADER_NAME}.spv
+                -capability ${SHADER_CAPABILITIES} -source-embed-style u32
+                -o ${SPIRV_DIR}/${SHADER_NAME}.spv
                 ${SHADER_SOURCE})
 
         add_custom_command(
@@ -39,8 +45,8 @@ function(add_shaders TARGET_NAME)
 
         add_custom_target(${SHADER_NAME} DEPENDS ${SPIRV_DIR}/${SHADER_NAME}.spv.h)
 
-        add_dependencies(${TARGET_NAME} ${SHADER_NAME})
+        add_dependencies(${ADD_SHADER_TARGET_NAME} ${SHADER_NAME})
     endforeach ()
 
-    target_include_directories(${TARGET_NAME} PRIVATE ${SPIRV_DIR})
+    target_include_directories(${ADD_SHADER_TARGET_NAME} PRIVATE ${SPIRV_DIR})
 endfunction()
